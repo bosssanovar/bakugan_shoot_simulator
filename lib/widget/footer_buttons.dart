0a6f3e8213c3f88d432486499409111f620f67ca
bosssanovar <bosssanovar@yahoo.co.jp>
2020-07-23 17:39:07 +0900

    currentAreaをウィジェットに抽出。

--- lib/widget/footer_buttons.dart
+++ lib/widget/footer_buttons.dart
@@ -1,21 +1,19 @@
+import 'package:bakugan_shoot_simulator/bloc/main_bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FooterButtons extends StatefulWidget {
-  FooterButtons(
-      {
-        Key key,
-        this.onShootBakugans,
-      }) : super(key: key) {
-    if(onShootBakugans == null){
-      throw ArgumentError();
-    }
-  }
+  const FooterButtons({
+    Key key,
+    this.bloc,
+    this.onUpdate,
+  }) : super(key: key);
 
   @override
   _FooterButtonsState createState() => _FooterButtonsState();
 
-  final Function onShootBakugans;
+  final MainBloc bloc;
+  final Function(Function) onUpdate;
 }
 
 class _FooterButtonsState extends State<FooterButtons> {
@@ -28,7 +26,9 @@ class _FooterButtonsState extends State<FooterButtons> {
           padding: const EdgeInsets.all(8),
           child: FloatingActionButton(
             onPressed: () {
-              widget.onShootBakugans();
+              widget.onUpdate(() {
+                widget.bloc.shootBakugans();
+              });
             },
             tooltip: 'shoot',
             child: Icon(Icons.refresh),
