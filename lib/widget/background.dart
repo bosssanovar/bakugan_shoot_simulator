41c8370eedce9beb6485470877322a32f1647cb5
bosssanovar <bosssanovar@yahoo.co.jp>
2020-07-24 20:53:53 +0900

    背景画像を軽量化

--- lib/widget/background.dart
+++ lib/widget/background.dart
@@ -5,13 +5,42 @@ class Background extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        buildWallpaper(),
+//        buildButtonsBackground()
+      ],
+    );
+  }
+
+  Widget buildWallpaper() {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('asset/background.png'),
-          fit: BoxFit.cover,
-        ),
+            image: AssetImage('asset/background.png'),
+            fit: BoxFit.none,
+            repeat: ImageRepeat.repeat),
       ),
     );
+  }
+
+  Widget buildButtonsBackground() {
+    return CustomPaint(painter: _MyPainter());
+  }
+}
+
+class _MyPainter extends CustomPainter {
+  @override
+  bool shouldRepaint(_MyPainter oldDelegate) {
+    return false;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final stroke = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3;
+    canvas.drawCircle(const Offset(0, 0), 100, stroke);
   }
 }
