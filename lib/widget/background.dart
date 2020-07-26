fbc2fa0ce191c2f2a9291c23692769ac39ff6193
bosssanovar <bosssanovar@yahoo.co.jp>
2020-07-26 10:11:42 +0900

    図形描画を確認中。

--- lib/widget/background.dart
+++ lib/widget/background.dart
@@ -8,7 +8,7 @@ class Background extends StatelessWidget {
     return Stack(
       children: <Widget>[
         buildWallpaper(),
-//        buildButtonsBackground()
+       // buildButtonsBackground()
       ],
     );
   }
@@ -25,22 +25,36 @@ class Background extends StatelessWidget {
   }
 
   Widget buildButtonsBackground() {
-    return CustomPaint(painter: _MyPainter());
+    return CustomPaint(
+      painter: _MyPainter(),
+      child: Container(
+        height: 50,
+      ),
+    );
   }
 }
 
 class _MyPainter extends CustomPainter {
-  @override
-  bool shouldRepaint(_MyPainter oldDelegate) {
-    return false;
-  }
 
   @override
   void paint(Canvas canvas, Size size) {
-    final stroke = Paint()
-      ..color = Colors.white
+    var paint = Paint();
+
+    // header buttons area
+    paint = new Paint()
+      ..color = Colors.teal
+      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 3;
-    canvas.drawCircle(const Offset(0, 0), 100, stroke);
+      ..strokeWidth = 2;
+    var path = Path();
+    path.moveTo(size.width / 3, size.height / 5 * 3);
+    path.lineTo(size.width / 3, size.height / 5 * 4);
+    path.lineTo(size.width / 3 * 2, size.height / 5 * 4);
+    path.lineTo(size.width / 3 * 2, size.height / 5 * 3);
+    path.close();
+    canvas.drawPath(path, paint);
   }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
