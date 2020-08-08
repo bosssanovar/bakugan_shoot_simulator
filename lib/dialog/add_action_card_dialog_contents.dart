3c1093c08452c176dc9c5028fd136819c428c981
bosssanovar <bosssanovar@yahoo.co.jp>
2020-08-08 12:02:23 +0900

    アクションカード追加機能対応。デザイン仮

--- lib/dialog/add_action_card_dialog_contents.dart
+++ lib/dialog/add_action_card_dialog_contents.dart
@@ -2,6 +2,7 @@ import 'package:bakugan_shoot_simulator/bloc/main_bloc.dart';
 import 'package:bakugan_shoot_simulator/dialog/ok_cancel_dialog_contents.dart';
 import 'package:bakugan_shoot_simulator/model/team/team_position.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class AddActionCardDialogContents extends OkCancelDialogContents {
 
@@ -37,41 +38,169 @@ class _AddActionCardDialogContentsState
 
   @override
   Widget build(BuildContext context) {
+    const h = 250.0;
     return Stack(
       children: <Widget>[
-        Container(),
         Container(
-            color: Colors.grey,
-            child: Center(
-              child: Column(
+          height: h,
+          color: Colors.grey,
+        ),
+        Container(
+            height: h,
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: 150,
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    'BP : ${battlePoint}',
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .headline,
+                  _buildDamageRate(),
+                  const SizedBox(
+                    width: 50,
                   ),
-                  Text(
-                    'こんな感じでダイアログが出せるよ',
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .headline,
-                  ),
-                  FlatButton(
-                    child: const Text('+100'),
-                    onPressed: () {
-                      setState(() {
-                        battlePoint += 100;
-                      });
-                    },
-                  )
+                  _buildBattlePoint(),
                 ],
               ),
             ))
       ],
+    );
+  }
+
+  Widget _buildDamageRate() {
+    return Column(
+      children: <Widget>[
+        _buildDamageRateDisplay(),
+        SizedBox(
+          height: 10,
+        ),
+        _buildDamageRateInput(),
+      ],
+    );
+  }
+
+  Widget _buildDamageRateInput() {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            MyRaisedButton('+1', () {
+              damageRate += 1;
+            }),
+            const SizedBox(width: 10),
+            MyRaisedButton('+5', () {
+              damageRate += 5;
+            }),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: <Widget>[
+            MyRaisedButton('-1', () {
+              damageRate -= 1;
+            }),
+            const SizedBox(width: 10),
+            MyRaisedButton('-5', () {
+              damageRate -= 5;
+            }),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDamageRateDisplay() {
+    return Column(
+      children: <Widget>[
+        Text(
+          'DR:${damageRate}',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBattlePoint() {
+    return Column(
+      children: <Widget>[
+        _buildBattlePointDisplay(),
+        SizedBox(
+          height: 10,
+        ),
+        _buildBattlePointInput(),
+      ],
+    );
+  }
+
+  Widget _buildBattlePointInput() {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            MyRaisedButton('+50', () {
+              battlePoint += 50;
+            }),
+            const SizedBox(width: 10),
+            MyRaisedButton('+100', () {
+              battlePoint += 100;
+            }),
+            const SizedBox(width: 10),
+            MyRaisedButton('+500', () {
+              battlePoint += 500;
+            }),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: <Widget>[
+            MyRaisedButton('-50', () {
+              battlePoint -= 50;
+            }),
+            const SizedBox(width: 10),
+            MyRaisedButton('-100', () {
+              battlePoint -= 100;
+            }),
+            const SizedBox(width: 10),
+            MyRaisedButton('-500', () {
+              battlePoint -= 500;
+            }),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBattlePointDisplay() {
+    return Column(
+      children: <Widget>[
+        Text(
+          'BP:${battlePoint}',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      ],
+    );
+  }
+
+  Widget MyRaisedButton(String text, Function onPressed) {
+    return SizedBox(
+      height: 50,
+      child: RaisedButton(
+        child: Text(
+          text,
+          style: Theme.of(context).textTheme.headline5,
+        ),
+        color: Colors.blue,
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        onPressed: () {
+          setState(() {
+            onPressed();
+          });
+        },
+      ),
     );
   }
 }
